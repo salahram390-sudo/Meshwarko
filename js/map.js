3424805d000b28876f6af49e7c78e44a1913525f
salahram390-sudo <salahram390@gmail.com>
2026-02-27 13:40:14 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -9,9 +9,9 @@ export function createMap(mapElId, options = {}) {
     .setView(options.center ?? [26.56, 31.70], options.zoom ?? 13);
 
   L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-  attribution: "© OpenStreetMap © CARTO",
   subdomains: "abcd",
-  maxZoom: 20
+  maxZoom: 20,
+  attribution: ""
 }).addTo(map);
   return map;
 }
