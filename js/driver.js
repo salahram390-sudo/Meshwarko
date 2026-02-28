57eac3cee9e65c23fbb2883c717dd4ef310b134c
salahram390-sudo <salahram390@gmail.com>
2026-02-28 04:19:38 +0200

    Update driver.js

--- js/driver.js
+++ js/driver.js
@@ -228,8 +228,18 @@ async function selectRide(id, ride) {
 
   try {
     const r = await routeOSRM(
-      { lat: ride.pickup.lat, lon: ride.pickup.lon },
-      { lat: ride.dropoff.lat, lon: ride.dropoff.lon }
+      const start = {
+  lat: ride.pickup.lat,
+  lon: ride.pickup.lon
+};
+
+const end = {
+  lat: ride.dropoff.lat,
+  lon: ride.dropoff.lon
+};
+
+const r = await routeOSRM(start, end);
+drawRoute(map, r.geojson, routeLayerRef);
     );
     drawRoute(map, r.geojson, routeLayerRef);
   } catch {}
