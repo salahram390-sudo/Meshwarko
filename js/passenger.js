2d50541d445a9ff346e268b27d55568e3eca2f67
salahram390-sudo <salahram390@gmail.com>
2026-02-28 23:25:45 +0200

    Update passenger.js

--- js/passenger.js
+++ js/passenger.js
@@ -1,4 +1,5 @@
-import { auth, db } from "./js/firebase.js";
+import { auth, db } from "./firebase.js";
+console.log("driver.js loaded ✅");
 let arrivedToastShownFor = null; // علشان ما تكررش الرسالة كل تحديث
 let myData = {};
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
