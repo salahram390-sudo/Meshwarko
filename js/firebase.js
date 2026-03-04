daa8ef48e1465e94207dd450a3136ca71fbea56f
salahram390-sudo <salahram390@gmail.com>
2026-03-04 09:24:04 +0200

    Expose auth and db to the global window object

--- js/firebase.js
+++ js/firebase.js
@@ -15,3 +15,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+window.__auth = auth;
+window.__db = db;
