d804a8a7525136e48253a58e07c546bed686786a
salahram390-sudo <salahram390@gmail.com>
2026-03-15 06:34:55 +0200

    Update passenger.js

--- js/passenger.js
+++ js/passenger.js
@@ -360,7 +360,7 @@ async function sendPassengerChatMessage() {
   senderRole: "passenger",
   senderName: myData.name || "الراكب",
   createdAt: serverTimestamp(),
-  typing:false
+  ,typing:false
   read: false
 });
 
