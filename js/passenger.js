92748a7a2c4ebf22b17ca58a65e3a6b5992dccca
salahram390-sudo <salahram390@gmail.com>
2026-03-24 16:25:58 +0200

    Update passenger.js

--- js/passenger.js
+++ js/passenger.js
@@ -686,6 +686,9 @@ function rideUiNone() {
 
   rideCard.innerHTML = `<div class="muted">لا يوجد طلب نشط.</div>`;
   setStatus("جاهز");
+  ratingValue = 0;
+if (rateComment) rateComment.value = "";
+setText(rateHint, "");
 }
 
 async function reverseNameEG(lat, lon) {
