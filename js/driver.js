186d8f0307c23b57550260f04a42af726293bbd1
salahram390-sudo <salahram390@gmail.com>
2026-03-25 09:10:46 +0200

    Update driver.js

--- js/driver.js
+++ js/driver.js
@@ -19,17 +19,6 @@ const meBadge = $("#meBadge");
 const logoutBtn = $("#logoutBtn");
 const switchRoleBtn = $("#switchRoleBtn");
 const editProfileBtn = $("#editProfileBtn");
-
-const menuBtnDriver = $("#menuBtnDriver");
-const drawerBackdropDriver = $("#drawerBackdropDriver");
-const sideDrawerDriver = $("#sideDrawerDriver");
-const drawerCloseDriver = $("#drawerCloseDriver");
-
-const drawerAccountDriver = $("#drawerAccountDriver");
-const drawerTripsDriver = $("#drawerTripsDriver");
-const drawerWalletDriver = $("#drawerWalletDriver");
-const drawerSupportDriver = $("#drawerSupportDriver");
-
 const driverAccountModal = $("#driverAccountModal");
 const driverAccountClose = $("#driverAccountClose");
 const driverAccountBackdrop = $("#driverAccountBackdrop");
