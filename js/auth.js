eb10d2e3aff314dd2e1f732f02a80d967b28b152
salahram390-sudo <salahram390@gmail.com>
2026-03-26 18:35:44 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -286,8 +286,10 @@ registerForm?.addEventListener("submit", async (e) => {
 
     await setDoc(doc(db, "users", cred.user.uid), { ...common, ...profile });
 
-    await ensureNotificationPermission(true);
-    location.href = role === "driver" ? "./driver.html" : "./passenger.html";
+await ensureNotificationPermission(true);
+await initFirebaseMessaging(cred.user.uid);
+
+location.href = role === "driver" ? "./driver.html" : "./passenger.html";
   } catch (err) {
     setText(regHint, friendlyAuthError(err));
   }
