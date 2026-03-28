47617fb9f5b83e15c937555a089f0d8d67991cfc
salahram390-sudo <salahram390@gmail.com>
2026-03-28 23:23:36 +0200

    Update driver.js

--- js/driver.js
+++ js/driver.js
@@ -15,6 +15,7 @@ import {
 } from "./map.js";
 import { loadEgyptAdmin } from "./admin_data.js";
 import { notify, ensureNotificationPermission, playSound, startRequestSound, stopRequestSound } from "./notify.js";
+localStorage.setItem("lastAppPage", location.pathname.split("/").pop() || "index.html");
 const meBadge = $("#meBadge");
 const logoutBtn = $("#logoutBtn");
 const switchRoleBtn = $("#switchRoleBtn");
