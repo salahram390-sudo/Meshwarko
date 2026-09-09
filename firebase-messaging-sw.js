efc8a17227d4ace04293828b8e3da1f9e9896244
salahram390-sudo <salahram390@gmail.com>
2026-09-09 16:48:14 +0300

    Update Firebase app version in service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-console.log("TEST");
+console.log("STEP 1 - SW START");
 
-importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
 
-console.log("FIREBASE TEST OK");
+console.log("STEP 2 - FIREBASE APP LOADED");
