48d563aa5c386024e11c5b7760cae32a1fb6b2a5
salahram390-sudo <salahram390@gmail.com>
2026-09-09 16:29:19 +0300

    Remove Firebase messaging setup from service worker
    Removed Firebase messaging initialization and background message handling.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,31 +1,7 @@
+console.log("STEP 1 - SW START");
+
 importScripts(
   "https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js"
 );
 
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js"
-);
-
-firebase.initializeApp({
-  apiKey: "AIzaSyBY72SkEi6HX4R9fSYAQhnYCLVbPu-W4Ko",
-  authDomain: "meshwarkomm.firebaseapp.com",
-  projectId: "meshwarkomm",
-  storageBucket: "meshwarkomm.firebasestorage.app",
-  messagingSenderId: "889669815551",
-  appId: "1:889669815551:web:b47e9dcf775e4c1eff10ca"
-});
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log("✅ BACKGROUND MESSAGE:", payload);
-
-  const title = payload.notification?.title || "مشوارك";
-  const body = payload.notification?.body || "لديك إشعار جديد";
-
-  self.registration.showNotification(title, {
-    body: body,
-    icon: "/Meshwarko/assets/logo.png",
-    badge: "/Meshwarko/assets/logo.png"
-  });
-});
+console.log("STEP 2 - FIREBASE APP LOADED");
