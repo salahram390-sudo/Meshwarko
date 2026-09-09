9cc952788750cdbdb2aab7cbbf9f7dbf9bbb003b
salahram390-sudo <salahram390@gmail.com>
2026-09-09 12:15:20 +0300

    Add Firebase messaging imports to passenger.js

--- js/passenger.js
+++ js/passenger.js
@@ -1,5 +1,4 @@
-
-import { auth, db } from "./firebase.js";
+import { auth, db, initFirebaseMessaging, listenForegroundMessages } from "./firebase.js";
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import {
   doc, getDoc, updateDoc,
