18996ebf1ea1d4de9647f2d574aa044736577150
salahram390-sudo <salahram390@gmail.com>
2026-09-22 16:44:42 +0300

    Update authorization header to use dynamic token

--- js/auth.js
+++ js/auth.js
@@ -365,7 +365,7 @@ try {
   const ADMIN_UID = "pW7aljtlVge5jR34akYTs9hBuwW2";
   await fetch("https://meshwarko-push.salahram390.workers.dev", {
     method: "POST",
-    headers: { "Content-Type": "application/json", "Authorization": "Bearer YVd/YhTgJ4Wwddeu88v/LqvSexujDKetSne/N537Hlo=" },
+    headers: { "Content-Type": "application/json", "Authorization": `Bearer ${await auth.currentUser.getIdToken()}`
     body: JSON.stringify({
       driverUids: [ADMIN_UID],
       title: role === "driver" ? "سائق جديد 🚗" : "راكب جديد 👤",
